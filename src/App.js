288b40206ea83a95b9c8c43d38dfab7febb2f940
manavpandya <pandya.manav@gmail.com>
2018-09-20 19:47:55 +0530

    updated form

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ class App extends Component {
   render() {
     return (
       <div className="App demoForm">
-        <h1>React Form Validations</h1><hr />
+        <h1>React Form Validations</h1><hr style={{ borderTop: '3px solid purple' }} />
         <SimpleForm />
-        <RegistrationForm />
+        {/* <RegistrationForm /> */}
       </div>
     );
   }
--- src/Forms/RegistrationForm.js
+++ src/Forms/RegistrationForm.js
@@ -1,37 +1,166 @@
 import React, { Component } from 'react';
 
 class RegistrationForm extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            formFields: {
+                firstName: '',
+                lastName: '',
+                email: '',
+                password: ''
+            },
+            errors: {}
+        }
+        // To bind different events
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    };
+
+    // When any field value changed
+    handleChange(event) {
+        let formFields = this.state.formFields;
+        formFields[event.target.name] = event.target.value;
+        this.setState({
+            formFields
+        });
+    }
+
+    // To validate all form fields
+    validate() {
+        let formFields = this.state.formFields;
+        let errors = {};
+        let IsValid = true;
+
+        if (!formFields["firstName"]) {
+            IsValid = false;
+            errors["firstName"] = "Enter Your First Name";
+        }
+
+        if (typeof formFields["firstName"] !== "undefined") {
+            if (!formFields["firstName"].match(/^[a-zA-Z ]*$/)) {
+                IsValid = false;
+                errors["firstName"] = "Only Alphabet Characters Are Allowed";
+            }
+        }
+
+        if (!formFields["lastName"]) {
+            IsValid = false;
+            errors["lastName"] = "Enter Your Last Name";
+        }
+
+        if (typeof formFields["lastName"] !== "undefined") {
+            if (!formFields["lastName"].match(/^[a-zA-Z ]*$/)) {
+                IsValid = false;
+                errors["lastName"] = "Only Alphabet Characters Are Allowed";
+            }
+        }
+
+        if (!formFields["email"]) {
+            IsValid = false;
+            errors["email"] = "Enter Your Email";
+        }
+
+        if (typeof formFields["email"] !== "undefined") {
+            var pattern = new RegExp(/^[a-z][a-zA-Z0-9_]*(\.[a-zA-Z][a-zA-Z0-9_]*)?@[a-z][a-zA-Z-0-9]*\.[a-z]+(\.[a-z]+)?$/);
+            if (!pattern.test(formFields["email"])) {
+                IsValid = false;
+                errors["email"] = "Invalid Email";
+            }
+        }
+
+        if (!formFields["password"]) {
+            IsValid = false;
+            errors["password"] = "Enter The Password";
+        }
+
+        if (typeof formFields["password"] !== "undefined") {
+            if (!formFields["password"].match("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})")) {
+                IsValid = false;
+                errors["password"] = "Enter Valid Password";
+            }
+        }
+
+        this.setState({
+            errors: errors
+        });
+        return IsValid;
+    }
+
+    // When user submits the form after validation
+    handleSubmit(event) {
+        event.preventDefault();
+        if (this.validate()) {
+            let formFields = {};
+            formFields["firstName"] = "";
+            formFields["lastName"] = "";
+            formFields["email"] = "";
+            formFields["password"] = "";
+            this.setState({ formFields: formFields });
+        }
+    }
+
     render() {
         return (
-            <div className="container" style={{ width: '430px' }}>
-                <h2>Simple Form For Registration</h2>
-                <form>
+            <div className="container" style={{ width: '315px' }}>
+                <h3>Employee Registration With Different Validations</h3>
+                <form onSubmit={this.handleSubmit} >
                     <div className="form-group">
-                        <label for="firstname">First Name</label>
-                        <input type="text" className="form-control" id="firstname" placeholder="Enter first name" name="firstname" />
-                    </div>
-                    <div className="form-group">
-                        <label for="lastname">Last Name</label>
-                        <input type="text" className="form-control" id="lastname" placeholder="Enter last name" name="lastname" />
-                    </div>
-                    <div className="form-group">
-                        <label for="email">Email</label>
-                        <input type="email" className="form-control" id="email" placeholder="Enter email" name="email" />
-                    </div>
-                    <div className="form-group">
-                        <label for="pwd">Password</label>
-                        <input type="password" className="form-control" id="pwd" placeholder="Enter password" name="pwd" />
+                        <label>First Name</label>
+                        <input className="form-control"
+                            type="text"
+                            name="firstName"
+                            value={this.state.formFields.firstName}
+                            onChange={this.handleChange} />
+                        {this.state.errors.firstName &&
+                            <div className="alert alert-danger">
+                                {this.state.errors.firstName}
+                            </div>
+                        }
                     </div>
                     <div className="form-group">
-                        <label for="cpwd">Confirm Password</label>
-                        <input type="password" className="form-control" id="cpwd" placeholder="Enter confirm password" name="cpwd" />
+                        <label>Last Name</label>
+                        <input className="form-control"
+                            type="text"
+                            name="lastName"
+                            value={this.state.formFields.lastName}
+                            onChange={this.handleChange} />
+                        {this.state.errors.lastName &&
+                            <div className="alert alert-danger">
+                                {this.state.errors.lastName}
+                            </div>
+                        }
                     </div>
-                    <div className="checkbox">
-                        <label><input type="checkbox" name="remember" />Remember me</label>
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input className="form-control"
+                            type="text"
+                            name="email"
+                            value={this.state.formFields.email}
+                            onChange={this.handleChange} />
+                        {this.state.errors.email &&
+                            <div className="alert alert-danger">
+                                {this.state.errors.email}
+                            </div>
+                        }
                     </div>
-                    <button type="submit" className="btn btn-default">Submit</button>
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input className="form-control"
+                            type="text"
+                            name="password"
+                            value={this.state.formFields.password}
+                            onChange={this.handleChange} />
+                        {this.state.errors.password &&
+                            <div className="alert alert-danger">
+                                {this.state.errors.password}
+                            </div>
+                        }
+                    </div>
+                    <input type="submit" className="btn btn-primary" value="Submit" />
                 </form>
-            </div>
+            </div >
         );
     }
 }
